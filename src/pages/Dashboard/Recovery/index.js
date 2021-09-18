9182be3579228f3fbc15db4e4f7a3a58aa5ef139
Anshuman Jaiswal <70937672+anshumanjaiswal@users.noreply.github.com>
2021-09-18 23:03:46 +0530

    Delete src/pages/Dashboard directory

--- src/pages/Dashboard/Recovery/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import EmailPassword from './../../Components/EmailPassword';
-
-const Recovery = (props) => {
-  return <EmailPassword/>
-    
-}
-
-export default Recovery;
--- src/pages/Dashboard/Registration/index.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import Signup from '../../Components/Signup';
-import './styles.scss';
-
-const Registration = props => {
-  return <Signup />;
-};
-
-export default Registration;
